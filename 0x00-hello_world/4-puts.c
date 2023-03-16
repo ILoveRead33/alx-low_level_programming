804ae28090e1b12049160f383059f4e76723938e
ILoveRead33 <saif.s36@yahoo.com>
2023-03-16 16:37:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+
+
 /**
  * main - prints *"Programming is like building a multilingual
- * puzzle, followed by a newline .
+ * puzzle, followed by a newline ."
  * Return: Always 0.
  */
 
